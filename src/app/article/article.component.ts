d632b3b38d7a158eab2f1aae156a3f0e712f2d55
Adolfo Mario Trevino Aguilar <adolfo@Adolfos-MacBook-Pro.local>
2020-06-15 22:27:11 -0500

    changes

---
+++ src/app/article/article.component.ts
@@ -0,0 +1,25 @@
+import { Component, OnInit, HostBinding, Input } from '@angular/core';
+import { Article } from './article.model';
+
+@Component({
+  selector: 'app-article',
+  templateUrl: './article.component.html',
+  styleUrls: ['./article.component.scss']
+})
+export class ArticleComponent implements OnInit {
+  @HostBinding('attr.class') cssClass = 'row';
+  @Input() article:Article;
+
+  constructor() {
+  }
+  voteUp(){
+    this.article.voteUp();
+  }
+  voteDown(){
+    this.article.voteDown();
+  }
+
+  ngOnInit(): void {
+  }
+
+}
